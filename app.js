7fbe05ccf90155589415a31bb020e2f85ac7b088
denise366 <denisesoledadaguero@gmail.com>
2024-02-27 22:43:55 -0300

    configuracion render

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require ("express")
 const app = express()
+const port = process.env.PORT || 3000;
 
-app.listen (3030, () => 
-            console.log ("corriendo puerto 3030"))
+
+
+/* inicializo PORT en render o servidor 3033 ruta localhost:3033 */
+
+app.listen (port, () => 
+    console.log(`Servidor corriendo en Puerto ${port}`)
+);
 const path = require ("path");
 app.use(express.static(path.resolve(__dirname,("./Public"))))
 app.get("/", function(req, res)
